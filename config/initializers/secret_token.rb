27b774d8ab0143a3cae74b66766b4a7e4b4ba179
Brian Lauber <constructible.truth@gmail.com>
2013-08-18 12:21:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmazonMonitor::Application.config.secret_key_base = 'db068a3d2a26a9f00dae23cbce5efe35a1a75293b33a2f34db33b57c3adce0eab2e20d1f45b563f10211cb035c4ad45279f13b0c68d37861ceb9f44a0336560e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmazonMonitor::Application.config.session_store :cookie_store, key: '_amazon_monitor_session'
